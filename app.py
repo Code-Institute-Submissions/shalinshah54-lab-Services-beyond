bd4a61504e5b523aec39a3cdb067dd439fd6a529
shalinshah54-lab <shalinshah54@hotmail.com>
2021-12-18 18:51:37 +0000

    changed the debug from true to false.

--- app.py
+++ app.py
@@ -162,4 +162,4 @@ def delete_service(service_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
